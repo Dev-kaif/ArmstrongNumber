7dd6d034e9673bab8f61b069f1eb3e3cf98a12e6
Mohammad Kaif <kaifghalib123@gmail.com>
2025-07-03 23:46:59 +0530

    added protection to user

--- Backend/src/routes/verificationRoutes.ts
+++ Backend/src/routes/verificationRoutes.ts
@@ -1,9 +1,10 @@
-import { Router } from 'express';
-import { getUserArmstrongNumbers, isItArmstrong } from '../controllers/armstrongController';
+import { Router } from "express";
+import {getUserArmstrongNumbers,isItArmstrong,} from "../controllers/armstrongController";
+import { protect } from "../middelwares/authmiddleware";
 
 const router = Router();
 
-router.post('/verify', isItArmstrong);
-router.get('/number/:id', getUserArmstrongNumbers);
+router.post("/verify", protect, isItArmstrong);
+router.get("/number/:id", protect, getUserArmstrongNumbers);
 
 export default router;
