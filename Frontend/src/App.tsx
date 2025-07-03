2dd5c6301ae4d0ced9c97c98f5d1a9a72ea19a79
Mohammad Kaif <kaifghalib123@gmail.com>
2025-07-04 04:04:41 +0530

    added routing

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -1,10 +1,25 @@
+import { Routes, Route } from "react-router-dom";
+import { Login } from "./components/Auth/Login";
+import { Signup } from "./components/Auth/Signup";
+import { DotPattern } from "./components/MagicUi/Backgroud";
+import { Toaster } from "./components/ui/sonner";
+import { cn } from "./lib/utils";
 
-function App() {
+export function App() {
   return (
-    <div>
-      
-    </div>
-  )
-}
+    <div className="w-full overflow-hidden min-h-screen">
+      <DotPattern
+        className={cn(
+          "[mask-image:radial-gradient(400px_circle_at_center,white,transparent)]",
+        )}
+      />
 
-export default App
+      <Routes>
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
+
+      <Toaster />
+    </div>
+  );
+}
