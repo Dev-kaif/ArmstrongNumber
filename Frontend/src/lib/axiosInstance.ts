c94dd3cd59620b1ef89148d15ed6722fdf018e72
Mohammad Kaif <kaifghalib123@gmail.com>
2025-07-04 06:32:01 +0530

    added axios instance and config

---
+++ Frontend/src/lib/axiosInstance.ts
@@ -0,0 +1,22 @@
+import axios from "axios";
+import { BACKEND_URL } from "./Config";
+
+const api = axios.create({
+  baseURL: `${BACKEND_URL}/api`,
+  headers: {
+    "Content-Type": "application/json",
+  },
+});
+
+api.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem("token");
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error) => Promise.reject(error)
+);
+
+export default api ;
